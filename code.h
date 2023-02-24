347b23b189f0dea46f25355270f4dbb360ba1de0
Carl Alphonce <alphonce@buffalo.edu>
2023-02-24 17:31:37 -0500

    Move struct definition from .c file

--- code.h
+++ code.h
@@ -2,9 +2,11 @@
 #include <string.h>
 #include <stdio.h>
 
-struct Node;
+struct Node {
+  int value;
+  struct Node * next;
+};
 
 void printList(struct Node *);
-
 void program1();
 struct Node * program2(int argc, char * argv[]);
